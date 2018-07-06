31bba94eb754fb7813e19e5cd8261a4316f4aba0
Noah Gribbin <noahgribin@yahoo.com>
2018-07-06 14:14:50 -0700

    navbar color fix

--- src/containers/contact.jsx
+++ src/containers/contact.jsx
@@ -15,10 +15,12 @@ class Contact extends React.Component {
         <h2 className="contact-title" >FIND US</h2>
         <div className="contact-container">
           <div className="contact-info">
-            <p>206 522 4601</p>
-            <p className="open-hours-label">Monday: Closed</p>
-            <p className="open-hours-label">Tuesday-Saturday 11:30–11:30 </p>
-            <p className="open-hours-label">Sunday 11:30–8:30 </p>
+            <div className="contact-info-center-wrapper">                
+              <p>206 522 4601</p>
+              <p className="open-hours-label">Monday: Closed</p>
+              <p className="open-hours-label">Tuesday-Saturday 11:30–11:30 </p>
+              <p className="open-hours-label">Sunday 11:30–8:30 </p>
+            </div>
           </div>
           <div className="iframe-container">
             <iframe
